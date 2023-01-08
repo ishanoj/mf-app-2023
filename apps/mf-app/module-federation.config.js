87af94e0ab8d638ce3fda8a5de2f087678dd348a
Shanoj <ishanoj@gmail.com>
2023-01-08 15:45:34 +0530

    mf code changes

---
+++ apps/mf-app/module-federation.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'mf-app',
+  exposes: {
+    './Module': 'apps/mf-app/src/app/remote-entry/entry.module.ts',
+  },
+};
---
+++ apps/mf-app/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mf-repo-mf-app-entry',
+  template: `<mf-repo-nx-welcome></mf-repo-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
